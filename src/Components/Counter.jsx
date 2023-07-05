d2d80b7cf2ce11a88f757fe5efe7e8f61bfbc5e5
Alaa El-faqawi <93674732+alaaelfaqawi@users.noreply.github.com>
2023-07-05 19:22:39 +0300

    project files

---
+++ src/Components/Counter.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './style.css'
+import Total from './Total';
+
+export default class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+  }
+
+  handleIncrement = () => {
+    const { value } = this.state;
+    const { onIncrement } = this.props;
+    const newValue = value + 1;
+    this.setState({ value: newValue });
+    onIncrement(newValue);
+  }
+
+  handleDecrement = () => {
+    const { value } = this.state;
+    const { onDecrement } = this.props;
+    const newValue = value - 1;
+    this.setState({ value: newValue });
+    onDecrement(newValue);
+  }
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <div>
+        <button onClick={this.handleIncrement}>+</button>
+        <span>{value}</span>
+        <button onClick={this.handleDecrement}>-</button>
+      </div>
+    );
+  }
+}
+
+<Total />
---
+++ src/Components/CounterApp.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Counter from './Counter';
+import Total from './Total';
+
+class CounterApp extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        counters: [0, 0, 0, 0]
+      };
+    }
+  
+    handleIncrement = (index) => {
+      const { counters } = this.state;
+      const newCounters = [...counters];
+      newCounters[index]++;
+      this.setState({ counters: newCounters });
+    }
+  
+    handleDecrement = (index) => {
+      const { counters } = this.state;
+      const newCounters = [...counters];
+      newCounters[index]--;
+      this.setState({ counters: newCounters });
+    }
+  
+    render() {
+      const { counters } = this.state;
+  
+      return (
+        <div>
+          <Counter
+            value={counters[0]}
+            onIncrement={(value) => this.handleIncrement(0, value)}
+            onDecrement={(value) => this.handleDecrement(0, value)}
+          />
+          <Counter
+            value={counters[1]}
+            onIncrement={(value) => this.handleIncrement(1, value)}
+            onDecrement={(value) => this.handleDecrement(1, value)}
+          />
+          <Counter
+            value={counters[2]}
+            onIncrement={(value) => this.handleIncrement(2, value)}
+            onDecrement={(value) => this.handleDecrement(2, value)}
+          />
+          <Counter
+            value={counters[3]}
+            onIncrement={(value) => this.handleIncrement(3, value)}
+            onDecrement={(value) => this.handleDecrement(3, value)}
+          />
+          <Total counters={counters} />
+        </div>
+      );
+    }
+  }
+  
+  export default CounterApp;
